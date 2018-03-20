a24a66ffd61883117cccd6755a9558a51aa3f203
Tyler Jaacks <tylerjaacks@Tylers-MacBook-Pro.local>
2018-03-20 12:02:24 -0500

    Setup the basic web server. Just need to get command arguments and sockets.

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
    Blog/Tutorial: https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-serial-communication-with-node-js/ 
 */
 var SerialPort = require('serialport');
-var Express = require('express');
+var WebSocketServer = require('ws').Server;
 
-var Server = Express();	
+var SERVER_PORT = 8081;
+
+var wss = new WebSocketServer({port: SERVER_PORT});
+var connections = new Array;
 
 // TODO Add a command argument checks here.
 // if (process.argv.length === 2) {
@@ -31,11 +34,6 @@ myPort.on('open', showPortOpen);
 myPort.on('close', showPortClose);
 myPort.on('error', showError);
 parser.on('data', readSerialData);
-
-Server.use('/', Express.static('www'));
-Server.listen(8080);
-
-sendData("Hello, world!");
 
 /* Gets a list of available ports on the system. */
 function getSerialPorts() {
@@ -64,4 +62,41 @@ function showError(error) {
 /* A callback function for reading data from a serial port.  */
 function readSerialData(data) {
     console.log(data);
+
+    if (connections.length > 0) {
+        broadcast(data);
+    }
+}
+
+/* Sends data to a serial port. */
+function sendToSerial(data) {
+    console.log("Sending to serial : " + data + ".");
+
+    myPort.write(data);
+}
+
+wss.on('connection', handleConnection);
+
+/* A callback function for when a new client connects to the socket. */
+function handleConnection(client) {
+    console.log("New connection.");
+
+    connections.push(client);
+
+    client.on('message', sendToSerial);
+
+    client.on('close', function() {
+        console.log("Connection closed.");
+
+        var position = connections.indexOf(client);
+
+        connections.splice(position, 1);
+    });
+}
+
+/* Broadcasts messages to all connected clients. */
+function broadcast(data) {
+    for (c in connections) {
+        connections[c].send(JSON.stringify(data));
+    }
 }
